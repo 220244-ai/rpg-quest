34c5633e6ce46f1b95039a241f2201579cc14ff1
coffin22 <220244@lhisd.net>
2025-08-25 08:56:26 -0500

    **Full Changelog**: https://github.com/220244-ai/rpg-quest/compare/v0.0.1...v0.1.2 ## Sprites - Added wolf sprite ## Mechanics - Added a life system - Player has 1 life - When the wolf overlaps with the player they lose a life - When player runs out of lives they lose - when the arrow overlaps with the wolf it destroys the wolf

--- main.ts
+++ main.ts
@@ -23,16 +23,30 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Points, function (sprite, otherS
     sprites.destroy(golden_nugget, effects.halo, 500)
     music.play(music.createSong(assets.song`golden_nugget_collected`), music.PlaybackMode.UntilDone)
 })
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+})
+info.onLifeZero(function () {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.dissolve)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(wolf, effects.disintegrate, 1000)
+})
 let arrow: Sprite = null
+let wolf: Sprite = null
 let golden_nugget: Sprite = null
 let player_1: Sprite = null
 tiles.setCurrentTilemap(tilemap`trial1`)
 player_1 = sprites.create(assets.image`player_sprite_1`, SpriteKind.Player)
 golden_nugget = sprites.create(assets.image`golden_nugget`, SpriteKind.Points)
+wolf = sprites.create(assets.image`wolf`, SpriteKind.Enemy)
 controller.moveSprite(player_1, 70, 70)
 player_1.setBounceOnWall(true)
-scene.cameraFollowSprite(player_1)
 player_1.setPosition(120, 120)
+scene.cameraFollowSprite(player_1)
+wolf.follow(player_1, 30)
+info.setLife(1)
 game.onUpdateInterval(randint(1000, 3000), function () {
     golden_nugget.setPosition(randint(0, 240), randint(0, 240))
 })
